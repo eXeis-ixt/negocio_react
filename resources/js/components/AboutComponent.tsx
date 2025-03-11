c450753a40d6284f9c4602a09f9167c02d5cd1af
eXeis-ixt <mjlickerlim56789@gmail.com>
2025-03-11 19:33:59 +0600

    changed text for about section

--- resources/js/components/AboutComponent.tsx
+++ resources/js/components/AboutComponent.tsx
@@ -10,50 +10,17 @@ const AboutComponent = () => {
                 <div className="w-full flex-col justify-center lg:items-start items-center gap-10 inline-flex">
                     <div className="w-full flex-col justify-center items-start gap-8 flex">
                         <div className="flex-col justify-start lg:items-start items-center gap-4 flex">
-                            <h6 className="text-gray-400  dark:text-gray-50 font-normal leading-relaxed">About Us</h6>
+                            <h6 className="text-gray-400  dark:text-gray-50 font-normal leading-relaxed">About Communic</h6>
                             <div className="w-full flex-col justify-start lg:items-start items-center gap-3 flex">
                                 <h2
                                     className="text-indigo-700 dark:text-white text-4xl font-bold font-manrope leading-normal lg:text-start text-center">
                                     The Tale of Our Achievement Story</h2>
                                 <p
                                     className="text-gray-500 dark:text-gray-100 text-base font-normal leading-relaxed lg:text-start text-center">
-                                    Our achievement story is a testament to teamwork and perseverance. Together, we've
-                                    overcome challenges, celebrated victories, and created a narrative of progress and
-                                    success.</p>
+                                   A platform, a learning hub, a student’s go-to practice ground is what defines COMMUNIC, the renowned club at IBA. The potential students of the prestigious Institute are continuously involved in learning, practicing & mastering fundamental & specialized communication skills through various programs, workshops, sessions & competitions. The Club aims not only to equip them with highly effective & efficient levels of diversified skills but also creates an entry-level ecosystem in the job world that will benefit the students with a leading edge in today’s competitive arena.</p>
                             </div>
                         </div>
-                        <div className="w-full flex-col justify-center items-start gap-6 flex">
-                            <div className="w-full justify-start items-center gap-8 grid md:grid-cols-2 grid-cols-1">
-                                <div
-                                    className="w-full h-full p-3.5 rounded-xl border border-gray-200 dark:border-zinc-600 hover:border-gray-400 transition-all duration-700 ease-in-out flex-col justify-start items-start gap-2.5 inline-flex">
-                                    <h4 className="text-gray-900 dark:text-white text-2xl font-bold font-manrope leading-9">33+ Years</h4>
-                                    <p className="text-gray-500 text-base font-normal leading-relaxed">Influencing Digital
-                                        Landscapes Together</p>
-                                </div>
-                                <div
-                                    className="w-full h-full p-3.5 rounded-xl border border-gray-200 dark:border-zinc-600 hover:border-gray-400 transition-all duration-700 ease-in-out flex-col justify-start items-start gap-2.5 inline-flex">
-                                    <h4 className="text-gray-900 dark:text-white text-2xl font-bold font-manrope leading-9">125+ Projects
-                                    </h4>
-                                    <p className="text-gray-500 text-base font-normal leading-relaxed">Excellence Achieved
-                                        Through Success</p>
-                                </div>
-                            </div>
-                            <div className="w-full h-full justify-start items-center gap-8 grid md:grid-cols-2 grid-cols-1">
-                                <div
-                                    className="w-full p-3.5 rounded-xl border border-gray-200 dark:border-zinc-600 hover:border-gray-400 transition-all duration-700 ease-in-out flex-col justify-start items-start gap-2.5 inline-flex">
-                                    <h4 className="text-gray-900 dark:text-white text-2xl font-bold font-manrope leading-9">26+ Awards</h4>
-                                    <p className="text-gray-500 text-base font-normal leading-relaxed">Our Dedication to
-                                        Innovation Wins Understanding</p>
-                                </div>
-                                <div
-                                    className="w-full h-full p-3.5 rounded-xl border border-gray-200 dark:border-zinc-600 hover:border-gray-400 transition-all duration-700 ease-in-out flex-col justify-start items-start gap-2.5 inline-flex">
-                                    <h4 className="text-gray-900 dark:text-white text-2xl font-bold font-manrope leading-9">99% Happy
-                                        Clients</h4>
-                                    <p className="text-gray-500 text-base font-normal leading-relaxed">Mirrors our Focus on
-                                        Client Satisfaction.</p>
-                                </div>
-                            </div>
-                        </div>
+
                     </div>
                    <Button>
                     Learn more
